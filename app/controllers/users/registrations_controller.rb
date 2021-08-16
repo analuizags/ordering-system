64368b3e06b8979631d7bc4ec0603c48b8cf5ea3
Ana Luiza <gs.analuiza@gmail.com>
2021-08-16 17:57:28 -0300

    Restringir acesso ao cadastro de usuário/restaurante

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,40 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_filter :configure_permitted_parameters
+  before_action :configure_permitted_parameters
+  before_action :check_permission, only: [:new, :create, :edit, :update]
 
   def new
     build_resource({})
     resource.build_restaurant
     respond_with self.resource
+  end
+
+  def create
+    super
+  end
+
+  def edit
+    super
+  end
+
+  def update
+    super
+  end
+
+  def destroy
+    super
+  end
+
+  # GET /resource/cancel
+  # Forces the session data which is usually expired after sign
+  # in to be expired now. This is useful if the user wants to
+  # cancel oauth signing in/up in the middle of the process,
+  # removing all OAuth session data.
+  # def cancel
+  #   super
+  # end
+
+  def check_permission
+    admin_signed_in? || redirect_to(new_admin_session_path)
   end
 
   protected
